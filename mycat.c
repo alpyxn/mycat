36b89d33368a92aac2e06cc752b68efd88840595
Alper Rasim Gürsoy <120681295+alpyxn@users.noreply.github.com>
2024-09-13 23:53:01 +0300

    Add files via upload
    First commit hehe

---
+++ mycat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: mycat file.txt\n");
+    return EXIT_FAILURE;
+  }
+
+  FILE *fs = fopen(argv[1], "r");
+
+  if (fs == NULL) {
+
+    fprintf(stderr, "Error: file could not be found.");
+    return EXIT_FAILURE;
+  }
+
+  char buffer[5000];
+
+  while (fgets(buffer, sizeof buffer, fs)) {
+    printf("%s", buffer);
+  }
+}
