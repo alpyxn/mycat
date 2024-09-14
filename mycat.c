49ff4bf85005d0d75d706ec48c47835a6b30b550
Alper Rasim Gürsoy <120681295+alpyxn@users.noreply.github.com>
2024-09-14 14:01:27 +0300

    Add files via upload
    Added creating copied text file via > operator.

--- mycat.c
+++ mycat.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 5000
 
 int main(int argc, char **argv) {
 
-  if (argc != 2) {
+  char buffer[BUFFER_SIZE];
+  if (argc < 2) {
     fprintf(stderr, "Usage: mycat file.txt\n");
     return EXIT_FAILURE;
   }
 
-  FILE *fs = fopen(argv[1], "r");
+  if (argc == 4 && strcmp(argv[2], ">") == 0) {
+    FILE *inputFile = fopen(argv[1], "r");
+    FILE *outputFile = fopen(argv[3], "w");
 
-  if (fs == NULL) {
+    if (inputFile == NULL) {
+      fprintf(stderr, "Error: file or files could not be created sorry.");
+      fclose(outputFile);
+      fclose(inputFile);
+      return EXIT_FAILURE;
+    }
+    if (outputFile == NULL) {
+      fprintf(stderr, "Error: file or files could not be created sorry.");
+      fclose(outputFile);
+      fclose(inputFile);
+      return EXIT_FAILURE;
+    }
+    while (fgets(buffer, sizeof buffer, inputFile)) {
+      fprintf(outputFile, "%s", buffer);
+    }
+    fclose(inputFile);
+    fclose(outputFile);
+    return EXIT_SUCCESS;
+  }
 
-    fprintf(stderr, "Error: file could not be found.");
+  else if (argc >= 2) {
+    for (int i = 1; i < argc; i++) {
+      FILE *fs = fopen(argv[i], "r");
+
+      if (fs == NULL) {
+        fprintf(stderr, "Error: %s file could not be found.", argv[i]);
+        return EXIT_FAILURE;
+      }
+
+      while (fgets(buffer, sizeof buffer, fs)) {
+        printf("%s", buffer);
+      }
+      fclose(fs);
+    }
+    return EXIT_SUCCESS;
+  }
+
+  else {
+    fprintf(stderr, "Usage: mycat file.txt\n");
     return EXIT_FAILURE;
   }
-
-  char buffer[5000];
-
-  while (fgets(buffer, sizeof buffer, fs)) {
-    printf("%s", buffer);
-  }
-
-  fclose(fs);
 }
